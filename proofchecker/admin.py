2cec413b655643417f83b28ebf681b74b6467d8c
raks <ra693@drexel.edu>
2021-10-12 20:45:02 -0400

    updated models on proofchecker

--- proofchecker/admin.py
+++ proofchecker/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Proof, Student, Instructor, Assignment, Course
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Proof, Student, Instructor, Assignment, Course,StudentAssignment
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
 admin.site.register(Proof)
 admin.site.register(Student)
 admin.site.register(Instructor)
 admin.site.register(Assignment)
 admin.site.register(Course)
+admin.site.register(StudentAssignment)
--- prooftool/settings.py
+++ prooftool/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-(*0ca2a@mdz2h%!hq-3#745=)d+%ommn!!xevozd2eov6f4($-
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -39,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'proofchecker',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'prooftool.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -86,7 +84,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -106,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -120,7 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -130,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# customized user model
+AUTH_USER_MODEL = 'proofchecker.User'
