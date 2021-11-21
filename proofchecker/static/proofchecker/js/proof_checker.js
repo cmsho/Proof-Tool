3046549bfe2dab80134548fd58318466de4cec96
Colton <cmshoenberger@gmail.com>
2021-11-21 07:05:15 -0500

    - Add delete button to proof_checker - Move proof_checker js to proof_checker.js

---
+++ proofchecker/static/proofchecker/js/proof_checker.js
@@ -0,0 +1,48 @@
+const addMoreBtn = document.getElementById("add-more")
+const totalNewForms = document.getElementById("id_form-TOTAL_FORMS")
+const currentProofLineForms = document.getElementsByClassName("proofline-form")
+addMoreBtn.addEventListener("click", add_new_form)
+
+function add_new_form(event){
+    if (event){
+        event.preventDefault()
+    }
+    let currentFormCount = currentProofLineForms.length
+    const formCopyTarget = document.getElementById("proof-line-list")
+    const emptyFormElement = document.getElementById("empty-form").cloneNode(true)
+    emptyFormElement.setAttribute("class", "proofline-form")
+    emptyFormElement.setAttribute("id", `form-${currentFormCount}`)
+    const regex = new RegExp('__prefix__', 'g')
+    emptyFormElement.innerHTML = emptyFormElement.innerHTML.replace(regex, currentFormCount)
+    totalNewForms.setAttribute('value', currentFormCount + 1)
+    formCopyTarget.append(emptyFormElement)
+}
+
+function delete_form(button){
+    // Delete the row
+    var id = button.id.replace(/[^0-9]/g, "")
+    console.log("Button ID: " + id)
+    const form_to_delete = document.getElementById("form-" + id)
+    console.log("Element to delete: " + form_to_delete)
+    form_to_delete.remove()
+
+    // Update the row count
+    const currentProofLineForms = document.getElementsByClassName("proofline-form")
+    let currentFormCount = currentProofLineForms.length
+    const totalNewForms = document.getElementById("id_form-TOTAL_FORMS")
+    totalNewForms.setAttribute('value', currentFormCount)
+}
+
+function replaceCharacter(ev) {
+    console.log(document.getElementById(ev.id));
+    let txt = document.getElementById(ev.id).value;
+    console.log(txt);
+
+    txt = txt.replace("\\and", "∧");
+    txt = txt.replace("\\or", "∨");
+    txt = txt.replace("\\implies", "→");
+    txt = txt.replace("\\not", "¬");
+    txt = txt.replace("\\iff", "↔");
+    txt = txt.replace("\\contradiction", "⊥");
+    document.getElementById(ev.id).value = txt;
+}
