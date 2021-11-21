4b11a1f7e3f89c26787d768744b3c68229e740c6
Colton <cmshoenberger@gmail.com>
2021-11-21 06:10:05 -0500

    Move add_edit script to add_edit.js

---
+++ proofchecker/static/proofchecker/js/add_edit.js
@@ -0,0 +1,36 @@
+const addMoreBtn = document.getElementById("add-more")
+const totalNewForms = document.getElementById("id_form-TOTAL_FORMS")
+const currentProofLineForms = document.getElementsByClassName("proofline-form")
+addMoreBtn.addEventListener("click", add_new_form)
+
+
+function add_new_form(button) {
+
+   let currentFormCount = currentProofLineForms.length
+   const formCopyTarget = document.getElementById("proof-line-list")
+
+   const emptyFormElement = document.getElementById("empty-form").cloneNode(true)
+   emptyFormElement.setAttribute("class", "proofline-form")
+   emptyFormElement.setAttribute("id", `proofline-form-${currentFormCount}`)
+
+   const regex = new RegExp('__prefix__', 'g')
+   emptyFormElement.innerHTML = emptyFormElement.innerHTML.replace(regex, currentFormCount)
+   totalNewForms.setAttribute('value', currentFormCount + 1)
+   formCopyTarget.append(emptyFormElement)
+}
+
+
+function removeProofline(button) {
+   var id = button.id.replace(/[^0-9]/g, "")
+   console.log(id)
+   const mark_deleted = document.getElementById("id_form-" + id + "-DELETE")
+   mark_deleted.setAttribute("checked", "true")
+   const targeted_prooflie = document.getElementById("proofline-form-" + id)
+   targeted_prooflie.hidden = true
+}
+
+const inputFields = document.getElementsByClassName("text-replacement-enabled")
+for (let i of inputFields){
+    console.log(i)
+    i.setAttribute("onkeydown", "replaceCharacter(this);")
+}
