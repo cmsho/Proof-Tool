8e2f363ebb48a65291293ba48c3a0851dd8ae938
rakhfa <ra693@drexel.edu>
2022-01-14 21:07:46 -0500

    removed space from FORMSET_TR_CLASS

--- proofchecker/static/proofchecker/js/proof_checker.js
+++ proofchecker/static/proofchecker/js/proof_checker.js
@@ -5,7 +5,7 @@
 const FORMSET_PREFIX                = "proofline_set";                 // for modelformset - "form-"
 const FORMSET_TOTALFORMS_ID         = "id_proofline_set-TOTAL_FORMS";   // for modelformset - "id_form-TOTAL_FORMS"
 const FORMSET_TBODY_ID              = "proofline-list";                 // for modelformset - "proofline-list"
-const FORMSET_TR_CLASS              = "proofline_set ";                 // for modelformset - "proofline-form"
+const FORMSET_TR_CLASS              = "proofline_set";                 // for modelformset - "proofline-form"
 
 // ---------------------------------------------------------------------------------------------------------------------
 
