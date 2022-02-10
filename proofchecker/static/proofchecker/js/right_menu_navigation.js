ceb6fe2eb708e60a209e2bfbb3073e2023769aec
tandrews75 <thomas.alton.andrews@gmail.com>
2022-02-09 21:46:00 -0500

    Removed other button in right nav, switched order, Renamed info to help

--- proofchecker/static/proofchecker/js/right_menu_navigation.js
+++ proofchecker/static/proofchecker/js/right_menu_navigation.js
@@ -4,33 +4,20 @@ window.onload = function () {
     const rightMenuSelection = localStorage.getItem("right-menu-selection");
 
     if (rightMenuSelection == "rules") {
-        document.getElementById("info").style.backgroundColor = "white";
         document.getElementById("rules").style.backgroundColor = "grey";
-        document.getElementById("other").style.backgroundColor = "white";
+        document.getElementById("help").style.backgroundColor = "white";
 
-        document.getElementById("info-information").style.display = "none";
         document.getElementById("rules-information").style.display = "block";
-        document.getElementById("other-information").style.display = "none";
+        document.getElementById("help-information").style.display = "none";
 
-    } else if (rightMenuSelection == "other") {
-        document.getElementById("info").style.backgroundColor = "white";
+    }
+
+    else if (rightMenuSelection == "help") {
         document.getElementById("rules").style.backgroundColor = "white";
-        document.getElementById("other").style.backgroundColor = "grey";
+        document.getElementById("help").style.backgroundColor = "grey";
 
-        document.getElementById("info-information").style.display = "none";
         document.getElementById("rules-information").style.display = "none";
-        document.getElementById("other-information").style.display = "block";
-
-
-
-    } else if (rightMenuSelection == "info") {
-        document.getElementById("info").style.backgroundColor = "grey";
-        document.getElementById("rules").style.backgroundColor = "white";
-        document.getElementById("other").style.backgroundColor = "white";
-
-        document.getElementById("info-information").style.display = "block";
-        document.getElementById("rules-information").style.display = "none";
-        document.getElementById("other-information").style.display = "none";
+        document.getElementById("help-information").style.display = "block";
 
     }
 };
@@ -42,37 +29,24 @@ function showInformation(e) {
 
     var button = document.getElementById(e);
 
-    if (e == 'info') {
-        document.getElementById("info-information").style.display = "block";
-        document.getElementById("rules-information").style.display = "none";
-        document.getElementById("other-information").style.display = "none";
-
-        document.getElementById("info").style.backgroundColor = "grey";
-        document.getElementById("rules").style.backgroundColor = "white";
-        document.getElementById("other").style.backgroundColor = "white";
-
-        localStorage.setItem("right-menu-selection", "info");
-
-    } else if (e == "rules") {
-        document.getElementById("info-information").style.display = "none";
+    if (e == "rules") {
         document.getElementById("rules-information").style.display = "block";
-        document.getElementById("other-information").style.display = "none";
+        document.getElementById("help-information").style.display = "none";
 
-        document.getElementById("info").style.backgroundColor = "white";
         document.getElementById("rules").style.backgroundColor = "grey";
-        document.getElementById("other").style.backgroundColor = "white";
+        document.getElementById("help").style.backgroundColor = "white";
 
         localStorage.setItem("right-menu-selection", "rules");
-    } else if (e == "other") {
-        document.getElementById("info-information").style.display = "none";
-        document.getElementById("rules-information").style.display = "none";
-        document.getElementById("other-information").style.display = "block";
-
-        document.getElementById("info").style.backgroundColor = "white";
-        document.getElementById("rules").style.backgroundColor = "white";
-        document.getElementById("other").style.backgroundColor = "grey";
-
-
-        localStorage.setItem("right-menu-selection", "other");
     }
+    else if (e == 'help') {
+        document.getElementById("rules-information").style.display = "none";
+        document.getElementById("help-information").style.display = "block";
+
+        document.getElementById("rules").style.backgroundColor = "white";
+        document.getElementById("help").style.backgroundColor = "grey";
+
+        localStorage.setItem("right-menu-selection", "help");
+
+    }
+
 }
