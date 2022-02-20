75a374c228ad1925f01c84bc91c19f13e83d214b
rakhfa <ra693@drexel.edu>
2022-02-19 22:57:44 -0500

    Crud for assignments with problems

--- assignments/urls.py
+++ assignments/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+
 from . import views
 
 admin.site.site_header = "ProofChecker Admin"
@@ -7,7 +8,8 @@ admin.site.site_header = "ProofChecker Admin"
 urlpatterns = [
     path("assignments/", views.AssignmentListView.as_view(), name="all_assignments"),
     path("assignments/add", views.create_assignment_view, name="add_assignment"),
-    path('assignments/<int:pk>/update', views.update_assignment_view, name="edit_assignment"),
+    path('assignment/<int:pk>/update', views.update_assignment_view, name="edit_assignment"),
+    path('assignment/<int:pk>/delete', views.AssignmentDeleteView.as_view(), name="delete_assignment"),
     path("problems/add", views.create_problem, name="add_problem"),
     path("problems/", views.ProblemView.as_view(), name="all_problems"),
     path('problems/<int:pk>/update', views.problem_update_view, name="edit_problem"),
--- assignments/views.py
+++ assignments/views.py
@@ -3,20 +3,24 @@ from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-from django.views.generic import ListView, CreateView, DeleteView
+from django.views.generic import ListView, DeleteView
 
 from proofchecker.forms import ProofLineForm
-from proofchecker.models import Proof, ProofLine, Problem, Assignment
+from proofchecker.models import Proof, ProofLine, Problem, Assignment, Instructor
 from .forms import AssignmentForm, ProblemForm, ProblemProofForm
+
 
 
 class AssignmentListView(ListView):
     model = Assignment
     template_name = "assignments/assignments.html"
 
+    def get_queryset(self):
+        return Assignment.objects.filter(created_by__user=self.request.user)
 
+
+@login_required
 def create_assignment_view(request):
-
     form = AssignmentForm(request.POST or None)
 
     context = {
@@ -25,7 +29,10 @@ def create_assignment_view(request):
 
     if request.POST:
         if form.is_valid():
-            assignment = form.save()
+            assignment = form.save(commit=False)
+            assignment.created_by = Instructor.objects.filter(user=request.user).first()
+            assignment.save()
+
             if request.htmx.current_url is not None:
                 return HttpResponse(assignment.id)
             else:
@@ -37,6 +44,7 @@ def create_assignment_view(request):
     return render(request, 'assignments/assignment_add_edit.html', context)
 
 
+
 def update_assignment_view(request, pk=None):
     assignment = get_object_or_404(Assignment, pk=pk)
     form = AssignmentForm(request.POST or None, instance=assignment)
@@ -44,7 +52,9 @@ def update_assignment_view(request, pk=None):
 
     if request.POST:
         if form.is_valid():
-            form.save()
+            assignment = form.save(commit=False)
+            assignment.problems.add(*problems)
+            assignment.save()
 
             if request.htmx.current_url is not None:
                 return HttpResponse(pk)
@@ -55,10 +65,18 @@ def update_assignment_view(request, pk=None):
                 return HttpResponse('')
 
     context = {
+        "assignment": assignment,
         "form": form,
-        "problems":problems
+        "problems": problems
     }
     return render(request, 'assignments/assignment_add_edit.html', context)
+
+
+
+class AssignmentDeleteView(DeleteView):
+    model = Assignment
+    template_name = "assignments/delete_assignment.html"
+    success_url = "/assignments/"
 
 
 @login_required
@@ -69,6 +87,50 @@ def create_problem(request):
     query_set = ProofLine.objects.none()
     ProofLineFormset = inlineformset_factory(Proof, ProofLine, form=ProofLineForm, extra=0, can_order=True)
     formset = ProofLineFormset(request.POST or None, instance=proof_form.instance, queryset=query_set)
+
+    assignmentPk = request.GET.get('assignment')
+    problem = None
+
+    if request.POST:
+        if all([problem_form.is_valid(), proof_form.is_valid(), formset.is_valid()]):
+            problem = problem_form.save(commit=False)
+            proof = proof_form.save(commit=False)
+
+            proof.created_by = request.user
+            proof.save()
+            formset.save()
+
+            problem.proof = proof
+            problem.save()
+
+            if assignmentPk is not None:
+                # problem page loaded from assignment page
+                assignment = Assignment.objects.get(id=assignmentPk)
+                assignment.problems.add(problem)
+                assignment.save()
+
+                return redirect("/assignment/" + assignmentPk + "/update")
+
+            return HttpResponseRedirect(reverse('all_problems'))
+
+    context = {
+        "problem_form": problem_form,
+        "proof_form": proof_form,
+        "formset": formset
+    }
+    return render(request, 'assignments/problem_add_edit.html', context)
+
+
+
+def problem_update_view(request, pk=None):
+    problem = get_object_or_404(Problem, pk=pk)
+    problem_form = ProblemForm(request.POST or None, instance=problem)
+
+    proof = Proof.objects.get(problem=problem)
+    proof_form = ProblemProofForm(request.POST or None, instance=proof)
+
+    ProofLineFormset = inlineformset_factory(Proof, ProofLine, form=ProofLineForm, extra=0, can_order=True)
+    formset = ProofLineFormset(request.POST or None, instance=proof, queryset=proof.proofline_set.order_by("ORDER"))
 
     assignmentPk = request.GET.get('assignment')
 
@@ -84,45 +146,10 @@ def create_problem(request):
             problem.proof = proof
             problem.save()
 
-            if (assignmentPk is not None):
-                assignment = Assignment.objects.get(id=assignmentPk)
-                assignment.problems.add(problem)
-                assignment.save()
+            if assignmentPk is not None:
+                # problem page loaded from assignment page
+                return redirect("/assignment/" + assignmentPk + "/update")
 
-                return redirect("/assignments/"+assignmentPk+"/update")
-
-
-            return HttpResponseRedirect(reverse('all_problems'))
-
-    context = {
-        "problem_form": problem_form,
-        "proof_form": proof_form,
-        "formset": formset
-    }
-    return render(request, 'assignments/problem_add_edit.html', context)
-
-
-def problem_update_view(request, pk=None):
-    problem = get_object_or_404(Problem, pk=pk)
-    problem_form = ProblemForm(request.POST or None, instance=problem)
-
-    proof = Proof.objects.get(problem=problem)
-    proof_form = ProblemProofForm(request.POST or None, instance=proof)
-
-    ProofLineFormset = inlineformset_factory(Proof, ProofLine, form=ProofLineForm, extra=0, can_order=True)
-    formset = ProofLineFormset(request.POST or None, instance=proof, queryset=proof.proofline_set.order_by("ORDER"))
-
-    if request.POST:
-        if all([problem_form.is_valid(), proof_form.is_valid(), formset.is_valid()]):
-            problem = problem_form.save(commit=False)
-            proof = proof_form.save(commit=False)
-
-            proof.created_by = request.user
-            proof.save()
-            formset.save()
-
-            problem.proof = proof
-            problem.save()
             return HttpResponseRedirect(reverse('all_problems'))
 
     context = {
@@ -134,12 +161,14 @@ def problem_update_view(request, pk=None):
     return render(request, 'assignments/problem_add_edit.html', context)
 
 
+
 class ProblemView(ListView):
     model = Problem
     template_name = "assignments/problems.html"
 
     # def get_queryset(self):
     #     return Problem.objects.filter(instructor__user=self.request.user)
+
 
 
 class ProblemDeleteView(DeleteView):
