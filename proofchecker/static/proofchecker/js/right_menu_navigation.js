8924ad53b4ea95ae863e788a2ade6dd78409059b
tandrews75 <thomas.alton.andrews@gmail.com>
2022-02-27 13:21:29 -0500

    Removed extraneous code/comments

--- proofchecker/static/proofchecker/js/right_menu_navigation.js
+++ proofchecker/static/proofchecker/js/right_menu_navigation.js
@@ -12,7 +12,6 @@ window.onload = function () {
 
         document.getElementById("rules-information").style.display = "block";
         document.getElementById("help-information").style.display = "none";
-
     }
 
     else if (rightMenuSelection == "help") {
@@ -31,14 +30,11 @@ window.onload = function () {
 
 
 function showInformation(e) {
-    console.log(e);
-
     var button = document.getElementById(e);
 
     if (e == "rules") {
         document.getElementById("rules-information").style.display = "block";
         document.getElementById("help-information").style.display = "none";
-
 
         document.getElementById("rules").classList.remove('btn-secondary');
         document.getElementById("rules").classList.add('btn-primary');
@@ -51,26 +47,20 @@ function showInformation(e) {
         document.getElementById("rules-information").style.display = "none";
         document.getElementById("help-information").style.display = "block";
 
-
         document.getElementById("rules").classList.remove('btn-primary');
         document.getElementById("rules").classList.add('btn-secondary');
         document.getElementById("help").classList.remove('btn-secondary');
         document.getElementById("help").classList.add('btn-primary');
 
         localStorage.setItem("right-menu-selection", "help");
-
     }
-
 }
 
 function showRules(e) {
-    console.log(e)
 
     if (e === "TFL-Button") {
-        console.log("TFL")
         document.getElementById("TFL-Rules").style.display = "block";
         document.getElementById("FOL-Rules").style.display = "none";
-
 
         document.getElementById("TFL-Button").classList.remove('btn-outline-secondary');
         document.getElementById("TFL-Button").classList.add('btn-outline-primary');
@@ -78,21 +68,16 @@ function showRules(e) {
         document.getElementById("FOL-Button").classList.add('btn-outline-secondary');
 
         localStorage.setItem("right-menu-selection", "TFL-Rules");
-
     }
     else if (e === "FOL-Button") {
-        console.log("FOL")
         document.getElementById("TFL-Rules").style.display = "none";
         document.getElementById("FOL-Rules").style.display = "block";
-
 
         document.getElementById("TFL-Button").classList.remove('btn-outline-primary');
         document.getElementById("TFL-Button").classList.add('btn-outline-secondary');
         document.getElementById("FOL-Button").classList.remove('btn-outline-secondary');
         document.getElementById("FOL-Button").classList.add('btn-outline-primary');
 
-
         localStorage.setItem("right-menu-selection", "FOL-Rules");
     }
-
 }
