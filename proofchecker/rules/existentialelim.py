8f9aa58173a9afad6e7cdf7dd91b307c7cfa660a
Colton Shoenberger <cmshoenberger@gmail.com>
2022-02-27 11:29:40 -0500

    Remove unused code

--- proofchecker/rules/existentialelim.py
+++ proofchecker/rules/existentialelim.py
@@ -8,7 +8,6 @@ class ExistentialElim(Rule):
 
     name = "Existential Elimination"
     symbols = "∃E"
-
     
     def verify(self, current_line: ProofLineObj, proof: ProofObj, parser):
         """
@@ -64,72 +63,6 @@ class ExistentialElim(Rule):
                     response.err_msg = "Error on line {}: The expressions on line {} and line {} should be equivalent"\
                         .format(str(current_line.line_no), str(target_lines[2].line_no), str(current_line.line_no))
                     return response
-
-                # # Verify line m and line i_1 use the same predicate
-                # if (root_m.right.value[0] != root_i_1.value[0]):
-                #     response.err_msg = "Error on line {}: Lines {} and {} should refer to the same predicate"\
-                #         .format(str(current_line.line_no), str(target_lines[0].line_no), str(target_lines[1].line_no))
-                #     return response
-
-                # # Verify that line m and current line have same number of predicate inputs
-                # count_m = 0
-                # count_curr = 0
-                # for ch in root_m.right.value:
-                #     if (is_var(ch) or is_name(ch)):
-                #         count_m += 1
-                # for ch in root_i_1.value:
-                #     if (is_var(ch) or is_name(ch)):
-                #         count_curr += 1
-
-                # if count_m != count_curr:
-                #     response.err_msg = "Error on line {}: The predicates on lines {} and {} do not have the same number of inputs"\
-                #         .format(str(current_line.line_no), str(target_lines[0].line_no), str(target_lines[1].line_no))
-                #     return response
-
-                # # Verify that each variable reference on line m is replaced by the same name on current line
-                # var = root_m.value[1]
-                # index = 0
-                # var_indexes = []
-
-                # # Eliminate whitespace in the functions to avoid issues
-                # func_m = root_m.right.value.replace(' ', '')
-                # func_i_1 = root_i_1.value.replace(' ', '')
-
-                # # Keep track of the locations (indexes) of the bound variable on line m
-                # for ch in func_m:
-                #     if ch == var:
-                #         var_indexes.append(index)
-                #     index += 1
-
-                # # Get the values at these locations in the current line
-                # # Make sure they all represent names
-                # names = []
-                # for i in var_indexes:
-                #     names.append(func_i_1[i])
-                
-                # for ch in names:
-                #     if not is_name(ch):
-                #         response.err_msg = "Error on line {}: Instances of variable {} on line {} should be replaced with a name on line {}"\
-                #             .format(str(current_line.line_no), var, str(target_lines[0].line_no), str(target_lines[1].line_no))
-                #         return response
-                
-                # # Make sure they all use the same name
-                # if len(names) > 1:
-                #     for ch in names:
-                #         if not ch == names[0]:
-                #             response.err_msg = "Error on line {}: All instances of variable {} on line {} should be replaced with the same name on line {}"\
-                #                 .format(str(current_line.line_no), var, str(target_lines[0].line_no), str(target_lines[1].line_no))
-                #             return response
-
-                # # Verify the expressions on line i_x and current line are equivalent
-                # # Eliminate whitespace to avoid issues
-                # func_i_x = expressions[2].replace(' ', '')
-                # func_curr = current_line.expression.replace(' ', '')
-
-                # if not func_i_x == func_curr:
-                #     response.err_msg = "Error on line {}: The expressions on line {} and line {} should be equivalent"\
-                #         .format(str(current_line.line_no), str(target_lines[2].line_no), str(current_line.line_no))
-                #     return response
 
                 response.is_valid = True
                 return response
