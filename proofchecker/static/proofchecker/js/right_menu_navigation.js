359d7451d30e014fb11a7634f2d561c2f10a8d3a
tandrews75 <thomas.alton.andrews@gmail.com>
2022-02-27 13:17:40 -0500

    Updated right hand menu with bootstrap buttons and changed layout

--- proofchecker/static/proofchecker/js/right_menu_navigation.js
+++ proofchecker/static/proofchecker/js/right_menu_navigation.js
@@ -4,8 +4,11 @@ window.onload = function () {
     const rightMenuSelection = localStorage.getItem("right-menu-selection");
 
     if (rightMenuSelection == "rules") {
-        document.getElementById("rules").style.backgroundColor = "grey";
-        document.getElementById("help").style.backgroundColor = "white";
+
+        document.getElementById("rules").classList.remove('btn-secondary');
+        document.getElementById("rules").classList.add('btn-primary');
+        document.getElementById("help").classList.remove('btn-primary');
+        document.getElementById("help").classList.add('btn-secondary');
 
         document.getElementById("rules-information").style.display = "block";
         document.getElementById("help-information").style.display = "none";
@@ -13,8 +16,11 @@ window.onload = function () {
     }
 
     else if (rightMenuSelection == "help") {
-        document.getElementById("rules").style.backgroundColor = "white";
-        document.getElementById("help").style.backgroundColor = "grey";
+
+        document.getElementById("rules").classList.remove('btn-primary');
+        document.getElementById("rules").classList.add('btn-secondary');
+        document.getElementById("help").classList.remove('btn-secondary');
+        document.getElementById("help").classList.add('btn-primary');
 
         document.getElementById("rules-information").style.display = "none";
         document.getElementById("help-information").style.display = "block";
@@ -33,8 +39,11 @@ function showInformation(e) {
         document.getElementById("rules-information").style.display = "block";
         document.getElementById("help-information").style.display = "none";
 
-        document.getElementById("rules").style.backgroundColor = "grey";
-        document.getElementById("help").style.backgroundColor = "white";
+
+        document.getElementById("rules").classList.remove('btn-secondary');
+        document.getElementById("rules").classList.add('btn-primary');
+        document.getElementById("help").classList.remove('btn-primary');
+        document.getElementById("help").classList.add('btn-secondary');
 
         localStorage.setItem("right-menu-selection", "rules");
     }
@@ -42,11 +51,48 @@ function showInformation(e) {
         document.getElementById("rules-information").style.display = "none";
         document.getElementById("help-information").style.display = "block";
 
-        document.getElementById("rules").style.backgroundColor = "white";
-        document.getElementById("help").style.backgroundColor = "grey";
+
+        document.getElementById("rules").classList.remove('btn-primary');
+        document.getElementById("rules").classList.add('btn-secondary');
+        document.getElementById("help").classList.remove('btn-secondary');
+        document.getElementById("help").classList.add('btn-primary');
 
         localStorage.setItem("right-menu-selection", "help");
 
+    }
+
+}
+
+function showRules(e) {
+    console.log(e)
+
+    if (e === "TFL-Button") {
+        console.log("TFL")
+        document.getElementById("TFL-Rules").style.display = "block";
+        document.getElementById("FOL-Rules").style.display = "none";
+
+
+        document.getElementById("TFL-Button").classList.remove('btn-outline-secondary');
+        document.getElementById("TFL-Button").classList.add('btn-outline-primary');
+        document.getElementById("FOL-Button").classList.remove('btn-outline-primary');
+        document.getElementById("FOL-Button").classList.add('btn-outline-secondary');
+
+        localStorage.setItem("right-menu-selection", "TFL-Rules");
+
+    }
+    else if (e === "FOL-Button") {
+        console.log("FOL")
+        document.getElementById("TFL-Rules").style.display = "none";
+        document.getElementById("FOL-Rules").style.display = "block";
+
+
+        document.getElementById("TFL-Button").classList.remove('btn-outline-primary');
+        document.getElementById("TFL-Button").classList.add('btn-outline-secondary');
+        document.getElementById("FOL-Button").classList.remove('btn-outline-secondary');
+        document.getElementById("FOL-Button").classList.add('btn-outline-primary');
+
+
+        localStorage.setItem("right-menu-selection", "FOL-Rules");
     }
 
 }
