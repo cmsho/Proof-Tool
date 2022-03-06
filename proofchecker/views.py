014f049bcb87b7ae283000691bb6480cbf7f1e7b
Colton Shoenberger <cmshoenberger@gmail.com>
2022-03-06 12:23:00 -0500

    Import redirect

--- proofchecker/views.py
+++ proofchecker/views.py
@@ -1,6 +1,6 @@
 from accounts.decorators import instructor_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
